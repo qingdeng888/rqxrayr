8016cede39c7c67982bc1d9c12f7ced9c330d8a8
qingdeng888 <35051730+qingdeng888@users.noreply.github.com>
2023-05-26 01:54:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,9 @@ sed -i "s#CloudID#${CloudID}#g;s#HTTPDZ#${HTTPDZ}#g" config.yml
 # 伪装 xrayr 执行文件
 RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
 mv v ${RELEASE_RANDOMNESS}
-
-# 如果有设置哪吒探针三个变量,会安装。如果不填或者不全,则不会安装
-[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && echo '0' | ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY}
-
+#运行哪吒
+[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ]
+./qcjk -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} &
 # 运行 nginx 和 v2ray
 nginx
-./${RELEASE_RANDOMNESS} -config=config.json
+./${RELEASE_RANDOMNESS} -config config.yml
