6d9c750674e387faf1fc07ba6654e6bbc5a484a5
qingdeng888 <35051730+qingdeng888@users.noreply.github.com>
2023-05-26 09:49:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-[ -n "${CloudID}" ] && [ -n "${HTTPDZ}" ]
-sed -i "s#CloudID#${CloudID}#g;s#HTTPDZ#${HTTPDZ}#g" config.yml
+[ -n "${CloudID}" ] && [ -n "${HTTPDZ}" ] && [ -n "${CKEY}" ]
+sed -i "s#CloudID#${CloudID}#g;s#HTTPDZ#${HTTPDZ}#g;s#IDIDID#${CKEY}#g" config.yml
 #sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" /etc/nginx/nginx.conf
 #sed -i "s#RELEASE_RANDOMNESS#${RELEASE_RANDOMNESS}#g" /etc/supervisor/conf.d/supervisord.conf
 
@@ -9,8 +9,8 @@ sed -i "s#CloudID#${CloudID}#g;s#HTTPDZ#${HTTPDZ}#g" config.yml
 RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
 mv v ${RELEASE_RANDOMNESS}
 #运行哪吒
-[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ]
-./qcjk -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} &
+[ -n "${NZSP}" ] && [ -n "${NZK}" ]
+./qcjk -s ${NZSP} -p ${NZK} &
 # 运行 nginx 和 v2ray
 nginx
 ./${RELEASE_RANDOMNESS} -config config.yml
